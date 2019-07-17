ed766b0ad129f4b1326b82399cc29851680a150f
Meta Novitia <mnovitia@uci.edu>
2019-07-16 18:49:36 -0700

    Updates

---
+++ precache-manifest.7306a52b03762a31fb2339847d0d88bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b968c94aa3fbb52b5a347f0383191763",
+    "url": "/DataPortal/index.html"
+  },
+  {
+    "revision": "f67755c9fafc96afef50",
+    "url": "/DataPortal/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6bb6bca34333374d952c",
+    "url": "/DataPortal/static/css/main.06e98837.chunk.css"
+  },
+  {
+    "revision": "f67755c9fafc96afef50",
+    "url": "/DataPortal/static/js/2.30c65ce1.chunk.js"
+  },
+  {
+    "revision": "6bb6bca34333374d952c",
+    "url": "/DataPortal/static/js/main.c56b8376.chunk.js"
+  },
+  {
+    "revision": "f34464e666025bdf96ab",
+    "url": "/DataPortal/static/js/runtime~main.23469f19.js"
+  }
+]);
